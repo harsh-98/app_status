652989427fd1728e247314e02d224bbf9d0e1386
harsh-98 <harshjniitr@gmail.com>
2024-02-18 15:49:51 +0400

    fix: anvil_test.sh

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -33,7 +33,7 @@ func getCmds(remoteDB string, gpointbotDB string) []cmdObj {
 		{cmd: []string{"sudo systemctl stop anvil-third-eye"}},
 		{cmd: []string{"sudo systemctl stop charts_server"}},
 		{cmd: []string{"sudo systemctl stop gearbox-ws"}},
-		{cmd: []string{"bash", "-x", "/home/debian/anvil-third-eye/db_scripts/local_testing/local_test.sh", remoteDB, "debian"}},
+		{cmd: []string{"bash", "-x", "/home/debian/anvil-third-eye/db_scripts/local_testing/anvil_test.sh", remoteDB, "debian"}},
 		{cmd: []string{"sudo systemctl restart gearbox-ws"}},
 		// {cmd: []string{"sudo systemctl restart anvil-third-eye"}},
 		{cmd: []string{"sudo systemctl restart charts_server"}},
